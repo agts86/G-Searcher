1908b8ab177c4ccf68f5094c002a3d5dee816c4d
agts86@gmail.com <36285803+SoichiroSato@users.noreply.github.com>
2025-03-07 23:13:43 +0900

    レスポンスフィールド追加

--- G-Searcher/Models/Dto/RestaurantDto.cs
+++ G-Searcher/Models/Dto/RestaurantDto.cs
@@ -1,39 +1,193 @@
+using System.Text.Json.Serialization;
+
 namespace G_Searcher.Models.Dto;
 
+/// <summary>
+/// グルメサーチAPIレスポンスのDTO
+/// </summary>
 public class RestaurantDto
 {
+    /// <summary>
+    /// 結果
+    /// </summary>
     public Result Results { get; set; }
 
+    /// <summary>
+    /// 結果
+    /// </summary>
     public class Result
     {
-        public Shops[] Shop { get; set; }
+        /// <summary>
+        /// APIのバージョン情報
+        /// </summary>
+        [JsonPropertyName("api_version")]
+        public string ApiVersion { get; set; }
+        
+        /// <summary>
+        /// 検索結果の全件数
+        /// </summary>
+        [JsonPropertyName("results_available")]
+        public int ResultsAvailable { get; set; }
+        
+        /// <summary>
+        /// このレスポンスに含まれる検索結果の件数
+        /// </summary>
+        [JsonPropertyName("results_returned")]
+        public string ResultsReturned { get; set; }
+        
+        /// <summary>
+        /// 検索結果の開始位置
+        /// </summary>
+        [JsonPropertyName("results_start")]
+        public int ResultsStart { get; set; }
+        
+        /// <summary>
+        /// 店舗情報の配列
+        /// </summary>
+        [JsonPropertyName("shop")]
+        public Shop[] Shops { get; set; }
 
-        public class Shops
+        /// <summary>
+        /// 店舗情報クラス
+        /// </summary>
+        public class Shop
         {
+            /// <summary>
+            /// 店舗ID
+            /// </summary>
+            [JsonPropertyName("id")]
+            public string Id { get; set; }
+            
+            /// <summary>
+            /// 掲載店名
+            /// </summary>
+            [JsonPropertyName("name")]
             public string Name { get; set; }
-
+            
+            /// <summary>
+            /// ロゴ画像URL
+            /// </summary>
+            [JsonPropertyName("logo_image")]
+            public string LogoImage { get; set; }
+            
+            /// <summary>
+            /// 住所
+            /// </summary>
+            [JsonPropertyName("address")]
             public string Address { get; set; }
+            
+            /// <summary>
+            /// 最寄駅名
+            /// </summary>
+            [JsonPropertyName("station_name")]
+            public string StationName { get; set; }
+            
+            /// <summary>
+            /// 緯度
+            /// </summary>
+            [JsonPropertyName("lat")]
+            public double Latitude { get; set; }
+            
+            /// <summary>
+            /// 経度
+            /// </summary>
+            [JsonPropertyName("lng")]
+            public double Longitude { get; set; }
+            
+            /// <summary>
+            /// お店のジャンル情報
+            /// </summary>
+            [JsonPropertyName("genre")]
+            public GenreInfo Genre { get; set; }
+            
+            /// <summary>
+            /// キャッチコピー
+            /// </summary>
+            [JsonPropertyName("catch")]
+            public string Catch { get; set; }
+            
+            /// <summary>
+            /// 交通アクセス
+            /// </summary>
+            [JsonPropertyName("access")]
+            public string Access { get; set; }
+            
+            /// <summary>
+            /// 店舗のURL情報
+            /// </summary>
+            [JsonPropertyName("urls")]
+            public UrlInfo Urls { get; set; }
+            
+            /// <summary>
+            /// 写真情報
+            /// </summary>
+            [JsonPropertyName("photo")]
+            public PhotoInfo Photo { get; set; }
 
-            public Genres Genre { get; set; }
-
-            public string Mobile_access { get; set; }
-
-            public Url Urls { get; set; }
-
-            public string Open { get; set; }
-            public string Close { get; set; }
-
-            public string Card { get; set; }
-
-            public string Non_Smoking { get; set; }
-            public class Genres
+            /// <summary>
+            /// お店のジャンル情報クラス
+            /// </summary>
+            public class GenreInfo
             {
+                /// <summary>
+                /// お店のジャンルコード
+                /// </summary>
+                [JsonPropertyName("code")]
+                public string Code { get; set; }
+                
+                /// <summary>
+                /// お店のジャンル名
+                /// </summary>
+                [JsonPropertyName("name")]
                 public string Name { get; set; }
             }
 
-            public class Url
+            /// <summary>
+            /// 店舗のURL情報クラス
+            /// </summary>
+            public class UrlInfo
             {
-                public string PC { get; set;}
+                /// <summary>
+                /// PC向けURL
+                /// </summary>
+                [JsonPropertyName("pc")]
+                public string Pc { get; set; }
+            }
+
+            /// <summary>
+            /// 写真情報クラス
+            /// </summary>
+            public class PhotoInfo
+            {
+                /// <summary>
+                /// PC向けの写真情報
+                /// </summary>
+                [JsonPropertyName("pc")]
+                public PhotoPc Pc { get; set; }
+            }
+
+            /// <summary>
+            /// PC向け写真情報クラス
+            /// </summary>
+            public class PhotoPc
+            {
+                /// <summary>
+                /// 店舗トップ写真（大）
+                /// </summary>
+                [JsonPropertyName("l")]
+                public string Large { get; set; }
+                
+                /// <summary>
+                /// 店舗トップ写真（中）
+                /// </summary>
+                [JsonPropertyName("m")]
+                public string Medium { get; set; }
+                
+                /// <summary>
+                /// 店舗トップ写真（小）
+                /// </summary>
+                [JsonPropertyName("s")]
+                public string Small { get; set; }
             }
         }
     }
