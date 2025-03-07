0f2b2df9d6476787bd99324ee46905600136e54a
agts86@gmail.com <36285803+SoichiroSato@users.noreply.github.com>
2025-03-08 02:44:28 +0900

    .NET9にバージョンアップ

--- G-Searcher/Program.cs
+++ G-Searcher/Program.cs
@@ -1,4 +1,5 @@
 using G_Searcher.Configurations;
+using Microsoft.AspNetCore.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,15 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
+    app.UseSwaggerUI(o => o.SwaggerEndpoint("/openapi/v1.json", "v1"));
 }
 
 app.UseMiddleware<Middleware>();
