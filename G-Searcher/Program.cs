6194648d145e602e8d77f9c5df56b063b788ebd3
agts86@gmail.com <36285803+SoichiroSato@users.noreply.github.com>
2025-03-07 22:19:33 +0900

    コンフィグ追加処理削除

--- G-Searcher/Program.cs
+++ G-Searcher/Program.cs
@@ -6,11 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-// appsettingsを読み込めるようにする
-IConfiguration config = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json")
-    .AddEnvironmentVariables()
-    .Build();
 
 var app = builder.Build();
 
