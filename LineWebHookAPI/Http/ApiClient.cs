ea3e4464818c3895410f1658187ff9be47d5cca6
agts86@gmail.com <36285803+SoichiroSato@users.noreply.github.com>
2025-03-10 22:10:48 +0900

    Http処理クラスとAPIClient基底クラスを作成

---
+++ LineWebHookAPI/Http/ApiClient.cs
@@ -0,0 +1,18 @@
+namespace LineWebHookAPI.Http;
+
+/// <summary>
+/// API通信クライアント基底クラス
+/// HttpClientは使い回すほうが効率がいいため、差し込み型にする
+/// </summary>
+public class ApiClient(HttpAdapter http,IConfiguration configuration)
+{
+    /// <summary>
+    /// HttpClient
+    /// </summary>
+    protected HttpAdapter Http { get; } = http;
+
+    /// <summary>
+    /// 設定情報
+    /// </summary>
+    protected IConfiguration Configuration { get; } = configuration;
+}
---
+++ LineWebHookAPI/Http/Http.cs
@@ -0,0 +1,86 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
+namespace LineWebHookAPI.Http;
+
+/// <summary>
+/// http操作クラス
+/// </summary>
+public class HttpAdapter
+{
+    /// <summary>
+    /// HttpClientオブジェクト
+    /// </summary>
+    private HttpClient HttpClient { get;}
+
+    /// <summary>
+    /// シリアライズオプション
+    /// </summary>
+    private JsonSerializerOptions JsonSerializerOptions { get; } = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public HttpAdapter(int timeout = 100000)
+    {
+        HttpClient = new HttpClient
+        {
+            Timeout = TimeSpan.FromMilliseconds(timeout)
+        };
+    }
+
+    /// <summary>
+    /// コンストラクタ
+    /// CookieとかAllowRedirectとかはここで設定する
+    /// </summary>
+    public HttpAdapter(HttpMessageHandler httpMessageHandler, int timeout = 100000)
+    {
+        HttpClient = new HttpClient(httpMessageHandler)
+        {
+            Timeout = TimeSpan.FromMilliseconds(timeout)
+        };
+    }
+
+    /// <summary>
+    /// Getリクエストをする
+    /// </summary>
+    /// <param name="url">リクエスト先</param>
+    /// <param name="authenticationHeaderValue">basic認証</param>
+    /// <returns>レスポンス結果</returns>
+    public async Task<T> GetAsync<T>(string url, AuthenticationHeaderValue authenticationHeaderValue = null)
+    {
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri(url)
+        };
+        request.Headers.Authorization = authenticationHeaderValue;
+        var res = await HttpClient.SendAsync(request);
+        if (!res.IsSuccessStatusCode) throw new HttpRequestException(res.RequestMessage.ToString());
+        var json = await res.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<T>(json, JsonSerializerOptions);
+    }
+
+    /// <summary>
+    /// Postリクエストをする
+    /// </summary>
+    /// <param name="url">リクエスト先</param>
+    /// <param name="contentType">ContentType</param>
+    /// <param name="authenticationHeaderValue">basic認証</param>
+    /// <returns>レスポンス結果</returns>
+    public async Task<T> PostRequestAsync<T, Tbody>(string url, Tbody body, AuthenticationHeaderValue authenticationHeaderValue = null)
+    {
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Post,
+            RequestUri = new Uri(url),
+            Content = new StringContent(JsonSerializer.Serialize(body), Encoding.UTF8, "application/json")
+        };
+        request.Headers.Authorization = authenticationHeaderValue;
+        var res = await HttpClient.SendAsync(request);
+        if (!res.IsSuccessStatusCode) throw new HttpRequestException(res.RequestMessage.ToString());
+        var json = await res.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<T>(json, JsonSerializerOptions);
+    }
+}
