4fe174633d4d84bec769d8637913d6e0cccfdf44
agts86@gmail.com <36285803+SoichiroSato@users.noreply.github.com>
2025-03-10 18:38:24 +0900

    Enum型が大文字を返すため小文字のコンバータをグローバルに登録

--- LineWebHookAPI/Program.cs
+++ LineWebHookAPI/Program.cs
@@ -1,4 +1,6 @@
 using LineWebHookAPI.Configurations;
+using System.Text.Json.Serialization;
+using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
 using LineWebHookAPI.Models.DB;
 
@@ -6,7 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
