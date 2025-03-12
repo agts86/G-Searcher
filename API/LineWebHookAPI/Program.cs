be6d46d1f51a4d3c7e92d2004a67379b72883353
agts86@gmail.com <36285803+SoichiroSato@users.noreply.github.com>
2025-03-13 01:41:18 +0900

    結局dockerの変数使わなかったので削除

--- API/LineWebHookAPI/Program.cs
+++ API/LineWebHookAPI/Program.cs
@@ -20,16 +20,12 @@ builder.Services.AddTransient<Middleware>();
 // Lineの署名検証用フィルター
 builder.Services.AddScoped<LineSignatureFilter>();
 
-// DockerコンテナのときのDB接続文字列
-var defaultConnection = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
-// appsettings.jsonのDB接続文字列
-var configConnection = builder.Configuration.GetConnectionString("DefaultConnection");
 // EFCoreの設定
 builder.Services.AddDbContext<MyContext>
 (
     // 一旦趣味だからSQLiteにしたけど
     // サービス展開考えたらdevとprodで分けるべき
-    options => options.UseSqlite(defaultConnection ?? configConnection)
+    options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
 var app = builder.Build();
