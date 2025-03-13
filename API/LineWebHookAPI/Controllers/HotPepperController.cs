12f8088083f91375f74b518650f374483545bb0f
agts86@gmail.com <36285803+SoichiroSato@users.noreply.github.com>
2025-03-14 03:58:23 +0900

    Revert "認証を戻して動くか試してみる"
    This reverts commit 8a972b2c8bf50012f70ef12532ba23583de2c7ec.

--- API/LineWebHookAPI/Controllers/HotPepperController.cs
+++ API/LineWebHookAPI/Controllers/HotPepperController.cs
@@ -3,7 +3,6 @@ using LineWebHookAPI.Models.BL.HotPeppers;
 using LineWebHookAPI.Models.Dto.HotPeppers;
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
-using LineWebHookAPI.Configurations;
 
 namespace LineWebHookAPI.Controllers;
 
@@ -36,7 +35,7 @@ public class HotPepperController
     /// <param name="gourmetGettingDto">位置情報</param>
     /// <returns>LineAPIにPostした内容</returns>
     [HttpPost("gourmet/location")]
-    [ServiceFilter(typeof(LineSignatureFilter))]
+    // [ServiceFilter(typeof(LineSignatureFilter))]
     public async Task<IActionResult> PostGourmetLocationAsync([FromBody] GourmetGettingDto gourmetGettingDto)
     {
         Console.WriteLine(JsonSerializer.Serialize(gourmetGettingDto));
