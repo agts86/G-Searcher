9ee4e24410d9032725bd76ce5130c40661b661cb
agts86@gmail.com <36285803+SoichiroSato@users.noreply.github.com>
2025-03-14 16:56:30 +0900

    これでもう一度認証チェックされるか試してみる

--- API/LineWebHookAPI/Controllers/HotPepperController.cs
+++ API/LineWebHookAPI/Controllers/HotPepperController.cs
@@ -35,7 +35,7 @@ public class HotPepperController
     /// <param name="gourmetGettingDto">位置情報</param>
     /// <returns>LineAPIにPostした内容</returns>
     [HttpPost("gourmet/location")]
-    // [ServiceFilter(typeof(LineSignatureFilter))]
+    [ServiceFilter(typeof(LineSignatureFilter))]
     public async Task<IActionResult> PostGourmetLocationAsync([FromBody] GourmetGettingDto gourmetGettingDto)
     {
         if (Env.IsDevelopment())
