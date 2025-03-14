0451a4444999b55c1bb34841fe796630e38bb2c2
agts86@gmail.com <36285803+SoichiroSato@users.noreply.github.com>
2025-03-14 23:07:05 +0900

    検索結果が1つ以上の条件に変更

--- API/LineWebHookAPI/Models/BL/HotPeppers/HotPepperBL.cs
+++ API/LineWebHookAPI/Models/BL/HotPeppers/HotPepperBL.cs
@@ -53,7 +53,7 @@ public class HotPepperBL(IConfiguration configuration, MyContext dbContext,IHost
         var Reply = new Reply()
         {
             ReplyToken = gourmetGettingDto.Events.First().ReplyToken,
-            Messages = columns.Length >= 0 ? 
+            Messages = columns.Length > 0 ? 
             new TemplateMessage[]
             {
                 new()
@@ -61,7 +61,7 @@ public class HotPepperBL(IConfiguration configuration, MyContext dbContext,IHost
                     AltText = "検索結果",
                     Template = new CarouselTemplate()
                     {
-                        Columns = gourmet.Results.ToCarouselTemplateColumns()
+                        Columns = columns
                     }
                 }
             } :
