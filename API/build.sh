1f762f2c57e97ec04dea151733b3ecd9dd613bf6
agts86 <agts86@gmail.com>
2025-04-03 16:56:38 +0900

    Revert "cloudflareにデプロイするためのシェル作成"
    This reverts commit 1a56a469388619783b507954be590f49109affd0.

--- API/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
-chmod +x dotnet-install.sh
-./dotnet-install.sh -c 9.0 -InstallDir ./dotnet
-./dotnet/dotnet --version
-./dotnet/dotnet publish "./LineWebHookAPI/LineWebHookAPI.csproj" -c Release -o output
