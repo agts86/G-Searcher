5d7ce17110e8d1e20c4d1f4c7b02dcdc88013744
agts86@gmail.com <36285803+SoichiroSato@users.noreply.github.com>
2025-04-11 18:43:34 +0900

    スペルをpowershellに合わせる

--- API/Dev-Certs-Link.bat
+++ API/Dev-Certs-Link.bat
@@ -1,4 +1,4 @@
 @echo off
-dotnet dev-certs https -ep %USERPROFILE%\.aspnet\https\WslLocalHost.pfx -p WslLocalHost
+dotnet dev-certs https -ep %USERPROFILE%\.aspnet\https\WslLocalhost.pfx -p WslLocalhost
 dotnet dev-certs https --trust
 setx WSLENV USERPROFILE/up
