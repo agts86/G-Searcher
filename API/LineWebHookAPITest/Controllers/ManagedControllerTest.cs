bdb446d5660f7c662d6c09381dc63a74a433e8c7
SoichiroSato <agts86@gmail.com>
2025-11-06 01:05:07 +0900

    CreatedAtおよびUpdatedAtプロパティの検証をUTC変換から現在時刻の比較に変更し、テストの精度を向上

--- API/LineWebHookAPITest/Controllers/ManagedControllerTest.cs
+++ API/LineWebHookAPITest/Controllers/ManagedControllerTest.cs
@@ -37,12 +37,12 @@ public class ManagedControllerTest : TestBase
         Assert.Equal(2, content.Length);
         Assert.Equal(0, content[0].Lat);
         Assert.Equal(1, content[0].Lng);
-        Assert.Equal(DateTime.Now.ToUniversalTime().AddHours(JapanKind).Date, content[0].CreatedAt.Date);
-        Assert.Equal(DateTime.Now.ToUniversalTime().AddHours(JapanKind).Date, content[0].UpdatedAt.Date);
+        Assert.Equal(DateTime.Now, content[0].CreatedAt, TimeSpan.FromMinutes(1));
+        Assert.Equal(DateTime.Now, content[0].UpdatedAt, TimeSpan.FromMinutes(1));
         Assert.Equal(1, content[1].Lat);
         Assert.Equal(0, content[1].Lng);
-        Assert.Equal(DateTime.Now.ToUniversalTime().AddHours(JapanKind).Date, content[1].CreatedAt.Date);
-        Assert.Equal(DateTime.Now.ToUniversalTime().AddHours(JapanKind).Date, content[1].UpdatedAt.Date);
+        Assert.Equal(DateTime.Now, content[1].CreatedAt, TimeSpan.FromMinutes(1));
+        Assert.Equal(DateTime.Now, content[1].UpdatedAt, TimeSpan.FromMinutes(1));
     }
 
     [Fact]
@@ -70,11 +70,11 @@ public class ManagedControllerTest : TestBase
         var content = (result as OkObjectResult).Value as GourmetWordLog[];
         Assert.Equal(2, content.Length);
         Assert.Equal("二郎系", content[0].Text);
-        Assert.Equal(DateTime.Now.ToUniversalTime().AddHours(JapanKind).Date, content[0].CreatedAt.Date);
-        Assert.Equal(DateTime.Now.ToUniversalTime().AddHours(JapanKind).Date, content[0].UpdatedAt.Date);
+        Assert.Equal(DateTime.Now, content[0].CreatedAt, TimeSpan.FromMinutes(1));
+        Assert.Equal(DateTime.Now, content[0].UpdatedAt, TimeSpan.FromMinutes(1));
         Assert.Equal("家系", content[1].Text);
-        Assert.Equal(DateTime.Now.ToUniversalTime().AddHours(JapanKind).Date, content[1].CreatedAt.Date);
-        Assert.Equal(DateTime.Now.ToUniversalTime().AddHours(JapanKind).Date, content[1].UpdatedAt.Date);
+        Assert.Equal(DateTime.Now, content[1].CreatedAt, TimeSpan.FromMinutes(1));
+        Assert.Equal(DateTime.Now, content[1].UpdatedAt, TimeSpan.FromMinutes(1));
     }
 
     [Fact]
@@ -101,10 +101,10 @@ public class ManagedControllerTest : TestBase
         var content = Assert.IsType<ErrorLog[]>(result.Value);
         Assert.Equal(2, content.Length);
         Assert.Equal("Error1", content[0].Contents);
-        Assert.Equal(DateTime.Now.ToUniversalTime().AddHours(JapanKind).Date, content[0].CreatedAt.Date);
-        Assert.Equal(DateTime.Now.ToUniversalTime().AddHours(JapanKind).Date, content[0].UpdatedAt.Date);
+        Assert.Equal(DateTime.Now, content[0].CreatedAt, TimeSpan.FromMinutes(1));
+        Assert.Equal(DateTime.Now, content[0].UpdatedAt, TimeSpan.FromMinutes(1));
         Assert.Equal("Error2", content[1].Contents);
-        Assert.Equal(DateTime.Now.ToUniversalTime().AddHours(JapanKind).Date, content[1].CreatedAt.Date);
-        Assert.Equal(DateTime.Now.ToUniversalTime().AddHours(JapanKind).Date, content[1].UpdatedAt.Date);
+        Assert.Equal(DateTime.Now, content[1].CreatedAt, TimeSpan.FromMinutes(1));
+        Assert.Equal(DateTime.Now, content[1].UpdatedAt, TimeSpan.FromMinutes(1));
     }
 }
