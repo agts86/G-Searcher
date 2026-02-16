8ca9c2adaba47286d3cc47ea1e6b4adddb02a99b
agts86 <agts86@gmail.com>
2026-02-17 02:24:39 +0900

    feat: マイグレーションクラスにコードカバレッジ除外属性を追加

---
+++ API/LineWebHookAPI/Migrations/MigrationsCoverageExclude.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace LineWebHookAPI.Migrations;
+
+[ExcludeFromCodeCoverage]
+public partial class InitialMigration
+{
+}
+
+[ExcludeFromCodeCoverage]
+public partial class AddRefreshToken
+{
+}
+
+[ExcludeFromCodeCoverage]
+public partial class LineWebHookContextModelSnapshot
+{
+}
